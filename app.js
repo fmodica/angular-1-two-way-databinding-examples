202c823040c31bb8bbf865701ff870255d80ef73
Frank Modica <fmodica85@gmail.com>
2016-12-03 17:17:54 -0500

    Added equals/digest error example

--- app.js
+++ app.js
@@ -81,4 +81,52 @@
             }
         };
     });
+
+    app.directive("customDirectiveWithEqualsAvoidingDigestError", function ($timeout) {
+        return {
+            scope: {
+                model: "=",
+                onChange: "&"
+            },
+            link: function (scope, $element, attrs, ngModelCtrl) {
+                var jQueryPluginApi = $element.thirdPartyJQueryPlugin().data("api");
+
+                scope.$watch(function () {
+                    return scope.model;
+                }, function () {
+                    alert("Rendering new value in customDirectiveWithEqualAvoidingDigestError directive: " + scope.model);
+                    jQueryPluginApi.setCount(scope.model);
+                });
+
+                $element.on("countUpdate", function () {
+                    // Simulate conditions for an "$apply already in progress" error.
+                    scope.$apply(function () {
+                        scope.model = jQueryPluginApi.getCount();
+
+                        // We cannot call $scope.$apply or we will get an "$apply already in progress" error.
+
+                        // Error!
+                        // scope.$apply(function () {
+                        //     scope.onChange();
+                        // });
+
+                        // $scope.evalAsync won't work either because it will evaluate inside the
+                        // the current digest cycle, after which the controller's value would be 
+                        // updated.
+
+                        // scope.$evalAsync(function () {
+                        //     // Model won't be updatd yet!
+                        //     scope.onChange();
+                        // });
+
+                        // We have to force another digest cycle in a way that does not trigger an error.
+                        // $timeout works but may cause flicker since this will be executed after the DOM renders.
+                        $timeout(function () {
+                            scope.onChange()
+                        });
+                    });
+                });
+            }
+        };
+    });
 })();
