c29b9fa8b9b621ef5fb6c70ac9743b6db8ca566c
Frank Modica <fmodica85@gmail.com>
2016-12-05 10:20:44 -0500

    Cleanup

--- app.js
+++ app.js
@@ -4,19 +4,34 @@
     var app = angular.module("myApp", []);
 
     app.controller("MyController", function ($scope) {
-        $scope.obj = {
+        $scope.countPrimitive = 1;
+
+        $scope.countObjectToBeEntirelyReplaced = {
+            count: 1
+        };
+
+        $scope.countObjectToHavePropertyUpdated = {
             count: 1
         };
 
         $scope.onChange = function () {
-            alert("Value in controller is " + $scope.obj.count);
+            alert(
+                "countPrimitive in controller is " + $scope.countPrimitive + ". " +
+                "countObjectToBeEntirelyReplaced.count in controller is " + $scope.countObjectToBeEntirelyReplaced.count + ". " +
+                "countObjectToHavePropertyUpdated.count in controller is " + $scope.countObjectToHavePropertyUpdated.count + "."
+            )
         };
     });
 
+    // This directive uses ng-model to accomplish two-way databinding to a 
+    // primitive (number).
     app.directive("ngModelBindingToPrimitive", function () {
         return {
             require: "ngModel",
-            scope: {},
+            scope: {
+                param1: "@",
+                param2: "@"
+            },
             link: function (scope, $element, attrs, ngModelCtrl) {
                 var jQueryPluginApi = $element.thirdPartyJQueryPlugin().data("api");
 
@@ -38,16 +53,63 @@
         };
     });
 
+    // This directive uses ng-model to accomplish two-way databinding to a 
+    // primitive (number). It avoids errors that occur when a digest is 
+    // already in progress.
     app.directive("ngModelBindingToPrimitiveAvoidingDigestError", function () {
         return {
             require: "ngModel",
-            scope: {},
+            scope: {
+                param1: "@",
+                param2: "@"
+            },
             link: function (scope, $element, attrs, ngModelCtrl) {
                 var jQueryPluginApi = $element.thirdPartyJQueryPlugin().data("api");
 
+                // This executes when the model changes (but not when this directive 
+                // updates the model with $setViewValue below).
                 ngModelCtrl.$render = function () {
+                    // Update the DOM (in this case we update thirdPartyJQueryPlugin).
                     console.log("Rendering new value in ngModelBindingToPrimitiveAvoidingDigestError directive: " + ngModelCtrl.$viewValue);
                     jQueryPluginApi.setCount(ngModelCtrl.$viewValue);
+                };
+
+                $element.on("countUpdate", function () {
+                    // Simulate a digest already in progress.
+                    scope.$apply(function () {
+                        // Update the model. Any "ng-change" callback will execute after the
+                        // model is updated. Use "$evalAsync" instead of "$apply" because the 
+                        // latter won't throw an "$apply already in progress" error if we are 
+                        // in the middle of a digest cycle, but it will schedule an additional 
+                        // digest cycle if needed.
+                        scope.$evalAsync(function () {
+                            ngModelCtrl.$setViewValue(jQueryPluginApi.getCount());
+                        });
+                    });
+                });
+            }
+        };
+    });
+
+    // This directive uses ng-model to accomplish two-way databinding to 
+    // an object. It avoids errors that occur when a digest is already
+    // in progress.
+    app.directive("ngModelBindingToObjectAvoidingDigestError", function () {
+        return {
+            require: "ngModel",
+            scope: {
+                param1: "@",
+                param2: "@"
+            },
+            link: function (scope, $element, attrs, ngModelCtrl) {
+                var jQueryPluginApi = $element.thirdPartyJQueryPlugin().data("api");
+
+                // This executes when the model changes (but not when this directive 
+                // updates the model with $setViewValue below).
+                ngModelCtrl.$render = function () {
+                    // Update the DOM (in this case we update thirdPartyJQueryPlugin).
+                    console.log("Rendering new value in ngModelBindingToObjectAvoidingDigestError directive: " + ngModelCtrl.$viewValue.count);
+                    jQueryPluginApi.setCount(ngModelCtrl.$viewValue.count);
                 };
 
                 $element.on("countUpdate", function () {
@@ -58,7 +120,9 @@
                         // digest cycle, but it will schedule an additional digest cycle if
                         // needed.
                         scope.$evalAsync(function () {
-                            ngModelCtrl.$setViewValue(jQueryPluginApi.getCount());
+                            ngModelCtrl.$setViewValue({
+                                count: jQueryPluginApi.getCount()
+                            });
                         });
                     });
                 });
@@ -66,11 +130,15 @@
         };
     });
 
+    // This directive uses scope "=" to accomplish two-way databinding to 
+    // a primitive (number).
     app.directive("equalsBindingToPrimitive", function () {
         return {
             scope: {
                 model: "=",
-                onChange: "&"
+                onChange: "&",
+                param1: "@",
+                param2: "@"
             },
             link: function (scope, $element, attrs, ngModelCtrl) {
                 var jQueryPluginApi = $element.thirdPartyJQueryPlugin().data("api");
@@ -99,18 +167,28 @@
         };
     });
 
+    // This directive uses scope "=" to accomplish two-way databinding to 
+    // a primitive (number). It avoids errors that occur when a digest is 
+    // already in progress.
     app.directive("equalsBindingToPrimitiveAvoidingDigestError", function ($timeout) {
         return {
             scope: {
                 model: "=",
-                onChange: "&"
+                onChange: "&",
+                param1: "@",
+                param2: "@"
             },
             link: function (scope, $element, attrs, ngModelCtrl) {
                 var jQueryPluginApi = $element.thirdPartyJQueryPlugin().data("api");
 
+                // Watch the model for changes. This will run both when the controller
+                // updates the model AND when this directive updates the model
+                // (potentially causing an unnecessary re-render of the DOM in the 
+                // latter case).
                 scope.$watch(function () {
                     return scope.model;
                 }, function () {
+                    // Update the DOM (in this case we update thirdPartyJQueryPlugin).
                     console.log("Rendering new value in equalsBindingToPrimitiveAvoidingDigestError directive: " + scope.model);
                     jQueryPluginApi.setCount(scope.model);
                 });
@@ -118,6 +196,9 @@
                 $element.on("countUpdate", function () {
                     // Simulate a digest already in progress.
                     scope.$apply(function () {
+                        // Update the model. Execute the onChange callback manually after
+                        // the model is updated. Note that our "$watch" function above will
+                        // run, potentially causing an unnecessary re-render of the DOM.
                         scope.model = jQueryPluginApi.getCount();
 
                         // We cannot call "$apply" to force a digest cycle because we will get
@@ -148,26 +229,38 @@
         };
     });
 
-    app.directive("equalsBindingToObject", function () {
+    // This directive uses scope "=" to accomplish two-way databinding to 
+    // an object. It updates a property on that object.
+    app.directive("equalsBindingToObjectUpdatingProperty", function () {
         return {
             scope: {
                 model: "=",
-                onChange: "&"
+                onChange: "&",
+                param1: "@",
+                param2: "@"
             },
             link: function (scope, $element, attrs, ngModelCtrl) {
                 var jQueryPluginApi = $element.thirdPartyJQueryPlugin().data("api");
 
+                // Watch the model for changes. This will run both when the controller
+                // updates the model AND when this directive updates the model
+                // (potentially causing an unnecessary re-render of the DOM in the 
+                // latter case).
                 scope.$watch(function () {
                     return scope.model.count;
                 }, function () {
-                    console.log("Rendering new value in equalsBindingToObject directive: " + scope.model.count);
+                    // Update the DOM (in this case we update thirdPartyJQueryPlugin).
+                    console.log("Rendering new value in equalsBindingToObjectUpdatingProperty directive: " + scope.model.count);
                     jQueryPluginApi.setCount(scope.model.count);
                 });
 
                 $element.on("countUpdate", function () {
-                    // Update the model. In this case the model is a copy of the reference
-                    // to the object in the controller. Thus we are updating properties on
-                    // the controller object directly.
+                    // Update the model. Execute the onChange callback manually after
+                    // the model is updated. Note that our "$watch" function above will
+                    // run, potentially causing an unnecessary re-render of the DOM. In 
+                    // this case the model is a copy of the reference to the object in
+                    // the controller. Thus we are updating properties on the controller 
+                    // object directly.
                     scope.$evalAsync(function () {
                         scope.model.count = jQueryPluginApi.getCount();
                         scope.onChange();
